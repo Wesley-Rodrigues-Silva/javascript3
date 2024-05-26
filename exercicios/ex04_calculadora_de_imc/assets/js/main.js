b4182793f2fa7365312f91dc3a0327ce2aa2369f
Wesley Rodrigues Silva <161177795+Wesley-Rodrigues-Silva@users.noreply.github.com>
2024-05-26 13:46:32 -0300

    att

--- exercicios/ex04_calculadora_de_imc/assets/js/main.js
+++ exercicios/ex04_calculadora_de_imc/assets/js/main.js
@@ -1,7 +1,8 @@
 const form = document.querySelector('form');
 const pesoInput = document.querySelector('#peso');
 const alturaInput = document.querySelector('#altura');
-const resultado = document.querySelector('#resultado')
+const resultado = document.querySelector('#resultado');
+const contItens = document.querySelector('.container-itens')
 
 form.addEventListener('submit', function(e){
     e.preventDefault();
@@ -17,17 +18,29 @@ function calcularImc(){
 
 
     if (imc < 18.5) {
-        resultado.textContent = `Seu IMC é: ${imc.toFixed(2)} (Abaixo do peso)`
+        resultado.textContent = `Seu IMC é: ${imc.toFixed(2)} (Abaixo do peso)`;
+        resultado.style.backgroundColor = '#dbce12';
+        contItens.style.color = '#0004ff';
     } else if (imc < 25) {
-        resultado.textContent = `Seu IMC é: ${imc.toFixed(2)} (Peso normal)`
+        resultado.textContent = `Seu IMC é: ${imc.toFixed(2)} (Peso normal)`;
+        resultado.style.backgroundColor = '#12db34';
+        contItens.style.color = '#0004ff';
     } else if (imc < 30) {
-        resultado.textContent = `Seu IMC é: ${imc.toFixed(2)} (Sobrepeso)`
+        resultado.textContent = `Seu IMC é: ${imc.toFixed(2)} (Sobrepeso)`;
+        resultado.style.backgroundColor = '#dbce12';
+        contItens.style.color = '#0004ff';
     } else if (imc < 35) {
         resultado.textContent = `Seu IMC é: ${imc.toFixed(2)} (Obesidade Grau I)`
+        resultado.style.backgroundColor = '#db6212';
+        contItens.style.color = '#0004ff';
     } else if (imc < 40) {
         resultado.textContent = `Seu IMC é: ${imc.toFixed(2)} (Obesidade Grau II)`
+        resultado.style.backgroundColor = '#db6212';
+        contItens.style.color = '#0004ff';
     } else {
         resultado.textContent = `Seu IMC é: ${imc.toFixed(2)} (Obesidade Grau III)`
+        resultado.style.backgroundColor = '#ff0000';
+        contItens.style.color = '#0004ff';
     }
         
 }
